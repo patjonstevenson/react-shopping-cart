aa4fa22bd7c62ecdefb7a1cbc363ead4df6f7b55
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-07 15:04:44 -0700

    completed both stretch goals: created a removeItem function and added data persistence with localStoragefor the cart; right now, removeItem removes all items with the given ID, but I want to make it so that it only removes one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -13,17 +13,28 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState(localStorage.getItem("cart")
+		? JSON.parse(localStorage.getItem("cart"))
+		: []);
+
+	useEffect(() => {
+		localStorage.setItem("cart", JSON.stringify(cart));
+	}, [cart])
 
 	const addItem = item => {
 		// add the given item to the cart
 		setCart([...cart, item]);
 	};
 
+	const removeItem = id => {
+		// remove the given item from the cart
+		setCart(cart.filter(item => item.id !== id));
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{ cart }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
 					<Navigation cart={cart} />
 
 					{/* Routes */}
