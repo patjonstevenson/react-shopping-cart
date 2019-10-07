1acc61833d1b55ec2e66b84a28a29355f9188ad6
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-07 14:46:31 -0700

    completed MVP

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 // Contexts
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -21,20 +22,21 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+				<CartContext.Provider value={{ cart }}>
+					<Navigation cart={cart} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
